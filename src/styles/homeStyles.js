abcaf0ec2d05441031c2cbeee50359fbdeba409c
Guy <guymunday@gmail.com>
2020-10-05 16:21:40 +0100

    box-shadow

--- src/styles/homeStyles.js
+++ src/styles/homeStyles.js
@@ -199,7 +199,7 @@ export const FeaturedVideo = styled.div`
 
 export const HomeShadow = styled.div`
   box-shadow: 3px 3px 4px #000;
-  background: var(--change);
+  /* background: var(--change); */
 `
 
 export const FeaturedProjects = styled.div`
